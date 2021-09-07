39d2369cab1e831348d24d1a12d2a6f5ae1cc0e9
Ok-786 <osamakiyani6123@gmail.com>
2021-09-07 08:59:02 +0500

    adasd

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Routes from './Routes';
 
 ReactDOM.render(
-        <Routes/>,
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Routes/>
+    </BrowserRouter>,
     document.querySelector('#root')
 )
